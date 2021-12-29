ad3e4ba2a169ada16eb38a9042cbf9cd64343245
Hassan Sh. AlShammari <hassanl22@hotmail.com>
2021-12-30 00:02:45 +0300

    Base of JS

---
+++ JS Project/js/myScript.js
@@ -0,0 +1,61 @@
+// create a variable called hello that holds the the value of a string "hello" using var.
+
+// create a variable called name that holds the value of a string "Your Name" using let.
+
+// Create a constant called pi that holds the value 3.14.
+
+// create a variable called elem that has the element with the id name
+
+// set the innerhtml of the elem with the value name that has been created before.
+
+// change the color of the elem to be white.
+
+// write a message in the console to let you know that the edits you made are done!
+
+// alert the user with a welcome message
+
+// create a variable called test with a value of 0
+
+// increase the value of test by 10
+
+// increase the value of test by 1.
+
+// use a different way of increasing the value of test by 1.
+
+// create an array that holds 3 of your favorite plates.
+
+// push another plate into the array.
+
+// sort that array.
+
+// revrese the array.
+
+// remove the last plate in the array.
+
+// print the elements of the array in the console using for loop.
+
+// ask the user to enter 2 numbers using prompt. Save the data inside of two variables, x and y.
+
+//Check if x and y are equal, x is bigger than y, or y is bigger than x. print sutiable messages to the console.
+
+// create a variable isON with the value true.
+
+// print false if the value of isON is false. True, otherwise. using ! operator.
+
+// create an object cat, with the keys: name, age, color, and cuteLevel.
+
+// create an array that holdes 3 objects. Use cat if you want.
+
+// using for loop, print the numbers in between of 0 and 10. Use continue if we reached 7, break if we reached 9. What do you notice?
+
+// Create a function called Hi that alert the used with a message.
+
+// Create a function called add that accept 2 parameters and return the sum of them.
+
+// use the function in a good way.
+
+// Create a function called submit, link it with the submit button in index.html.
+
+// Save the data inside of variables, check if all of the data are available.
+
+// print the data to the console.
